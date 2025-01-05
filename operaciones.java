e5ba0f6ce5f30e6f8d99e5eb51719622c988813c
mayraisa3000 <mayraisa3000@gmail.com>
2025-01-04 20:21:56 -0600

    codigo

---
+++ operaciones.java
@@ -0,0 +1,15 @@
+public class operaciones {
+    public static void main(String[] args ){
+        int N1 = 5;
+        int N2 = 10;
+        int N3 = 20;
+        int suma = N1+N2;
+        int Resta = N3-N1;
+        int Multi= N1*N3;
+        int Divi = N3/N2;
+        System.out.println("suma: "+suma);
+        System.out.println("resta: "+Resta);
+        System.out.println("multiplicacion: "+Multi);
+        System.out.println("division: "+Divi);
+    }
+}
